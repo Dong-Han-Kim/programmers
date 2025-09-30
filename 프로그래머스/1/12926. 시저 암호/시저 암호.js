940aaaeb9a415fbed5714fd6ee4328df4d35bc71
Dong Han <148463611+Dong-Han-Kim@users.noreply.github.com>
2025-09-30 16:33:15 +0900

    [level 1] Title: 시저 암호, Time: 5.22 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,23 @@
+function solution(s, n) {
+    let result = "";
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === " ") {
+            result += s[i];
+        } else {
+            let code = s.charCodeAt(i);
+        
+            if (code >= 65 && code <= 90 ) {
+                result += String.fromCharCode((code - 65 + n) % 26 + 65);
+            } 
+            
+            if(code >= 97 && code <= 122) {
+                result += String.fromCharCode((code - 97 + n) % 26 + 97);
+            } 
+        }
+        
+        
+    }
+    
+    return result;
+}
